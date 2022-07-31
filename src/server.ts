f0377a8039b813c7f222b0201ba41862c2b20e45
Diener Dornelas <diener.ld@outlook.com>
2022-07-31 15:50:20 -0300

    add .env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import { app } from "./app";
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+import { app } from './app';
+import 'dotenv/config';
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
 });
