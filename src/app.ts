24d76b3c5d557a9c009b75ce1a8736f8d608e24f
Diener Dornelas <diener.ld@outlook.com>
2022-08-03 15:55:07 -0300

    fix: fix cors

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
-import cors from 'cors'
 
-const whitelist = (process.env.ALLOWED_ORIGINS?.split(/,\s{1,}/)) || [];
+const whitelist = (process.env.ALLOWED_ORIGINS?.split(/,\s{1,}/)) || ['*'];
 const corsOptions: cors.CorsOptions = {
-  origin: (origin, callback) =>{
-    if(whitelist.indexOf('*') !== -1) {
-      callback(null, true)
-    }else if (origin && whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+  origin: (origin, callback) => {
+    if (whitelist.indexOf('*') !== -1) {
+      callback(null, true);
+    } else if (origin && whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
-  }
-}
+  },
+};
 
 export const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(routes)
+app.use(routes);
