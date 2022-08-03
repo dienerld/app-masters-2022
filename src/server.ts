9bf0faa13192a0489b18f0ce52c78bf6df2b69aa
Diener Dornelas <diener.ld@outlook.com>
2022-08-03 15:43:37 -0300

    test remove swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import 'dotenv/config';
-import swaggerUi from 'swagger-ui-express';
 import { app } from './app';
-import swaggerDocument from './doc/openapi.json';
+// import swaggerUi from 'swagger-ui-express';
+// import swaggerDocument from './doc/openapi.json';
 
 const PORT = process.env.PORT || 3000;
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
