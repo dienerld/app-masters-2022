c9d6b3b2386054c9714d8e0b0364f0cb4504ae44
Diener Dornelas <diener.ld@outlook.com>
2022-08-03 15:31:12 -0300

    style: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+};
