460cc5716c4f6e6c1e79d72a098b9fce26cdf670
Diener Dornelas <diener.ld@outlook.com>
2022-08-03 15:31:27 -0300

    style: add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import { app } from './app';
 import 'dotenv/config';
-import swaggerUi from 'swagger-ui-express'
-import swaggerDocument from './doc/openapi.json'
+import swaggerUi from 'swagger-ui-express';
+import { app } from './app';
+import swaggerDocument from './doc/openapi.json';
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => {
-  console.log('Server is running on port ' + PORT);
+  console.log(`Server is running on port ${PORT}`);
 });
