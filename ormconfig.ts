cbc74ed29587982c422605296cd5256b8666c20a
Diener Dornelas <diener.ld@outlook.com>
2022-08-04 17:47:27 -0300

    fix: add new enviroment

--- ormconfig.ts
+++ ormconfig.ts
@@ -10,14 +10,25 @@ const isTest = () => {
   }
   return {};
 };
+const setConnection = () => {
+  if (process.env.DATABASE_URL) {
+    return {
+      url: process.env.DATABASE_URL,
+    };
+  }
+
+  return {
+    host: process.env.TYPEORM_DB_HOST,
+    port: Number(process.env.TYPEORM_DB_PORT),
+    username: process.env.TYPEORM_DB_USERNAME,
+    password: process.env.TYPEORM_DB_PASSWORD,
+    database: process.env.TYPEORM_DB_DATABASE,
+  };
+};
 
 module.exports = {
   type: process.env.TYPEORM_DB_TYPE,
-  host: process.env.TYPEORM_DB_HOST,
-  port: Number(process.env.TYPEORM_DB_PORT),
-  username: process.env.TYPEORM_DB_USERNAME,
-  password: process.env.TYPEORM_DB_PASSWORD,
-  database: process.env.TYPEORM_DB_DATABASE,
+  ...setConnection(),
   ...isTest(),
   entities: ['./src/**/*.model.{js,ts}'],
   migrations: ['./src/database/migrations/*.{js,ts}'],
