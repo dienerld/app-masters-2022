4c8ffcad953cfbc3e46f5b566203da062e587bc2
Diener Dornelas <diener.ld@outlook.com>
2022-08-07 14:16:46 -0300

    update cors

--- src/app.ts
+++ src/app.ts
@@ -9,22 +9,12 @@ import 'express-async-errors';
 import { routes } from './routes';
 import { RequestCustomError } from './errors/requestError';
 
-const whitelist = (process.env.ALLOWED_ORIGINS?.split(/,\s{1,}/)) || [];
-const corsOptions: cors.CorsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf('*') !== -1) {
-      callback(null, true);
-    } else if (origin && whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
 database.create();
 
 export const app = express();
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json());
 app.use(routes);
 
